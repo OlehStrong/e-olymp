adf4459ac3d4f2b77a7286199ab8a449e17deb1e
OlehStrong <yavtushenkooleh1@gmail.com>
2020-10-11 16:47:53 +0300

    first commit

---
+++ Average.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	int a, b, c;
+	scanf("%d", &a);
+	scanf("%d", &b);
+	scanf("%d", &c);
+	if (a < b < c || a > b > c) {
+
+		printf("%d", b);
+	}
+	else if (b < a < c || b > a > c) {
+
+		printf("%d", a);
+	}
+	else if (b < c < a || b > c > a)
+	{	
+
+		printf("%d", c);
+	}
+}
---
+++ CD.cpp
@@ -0,0 +1,41 @@
+﻿// CD.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <stdio.h>
+
+int main()
+{
+	int n;
+
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+	scanf("%d", &n);
+	int price = 0;
+	if (n >= 70) {
+		if (n >= 100) {
+			price = 100 * (n / 100);
+			n = n - 100 * (n / 100);
+		}
+		else
+		{
+			price = price + 100;
+		}
+	
+	}
+	 if (n < 70 && n>15) {
+		if (n > 20) {
+			price = price + (30 * (n / 20));
+			n = n - (n / 20 * 20);
+		}
+		else {
+			price = price + 30;
+		}
+	}
+	if(n<=15){
+
+		price += n * 2;
+	}
+	printf("%d", price);
+    
+}
+
